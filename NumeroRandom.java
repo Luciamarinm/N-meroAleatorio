f5f9c360c327c9785a6969bedbfb7f1c69f6e987
Luciamarinm <72409783+Luciamarinm@users.noreply.github.com>
2020-10-15 17:05:48 +0200

    Update NumeroRandom.java

--- NumeroRandom.java
+++ NumeroRandom.java
@@ -7,28 +7,27 @@ public class NumeroRandom {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+
 		int aleatorio = (int) ((Math.random() * 100)+1);
-		int numero = 0;
+		int numero;
 		int intentos = 10;
-		
-		System.out.println("Acierta el número del 1 al 100, tienes 10 intentos  ");
-		
-		while (numero != aleatorio) {
-			intentos++;
-			numero = sc.nextInt();
-		
-			if (numero < aleatorio) {
-				System.out.println("El número es más alto, sigue probando"); }
-			else if (numero > aleatorio) {
-				System.out.println("El número es más bajo, sigue probando"); }
-			else if (intentos == 10) {
-				System.out.println("No has acertado, el número es "+ aleatorio);
+
+		System.out.println("Acierta el nÃºmero del 1 al 100, tienes 10 intentos  ");
+		numero = sc.nextInt();
+		while (numero != aleatorio && intentos >= 1) {
+			intentos = intentos -1;
+			if (numero > 100 || numero < 1) {
+				System.out.println("NÃºmero fuera de rango, elige entre 1 y 100"); }
+			if (numero == aleatorio) {
+				System.out.println("Has acertado"); }
+			else if (numero < aleatorio) {
+				System.out.println("El nÃºmero es mÃ¡s alto, quedan " + intentos + " intentos, sigue probando"); }
+			else {
+				System.out.println("El nÃºmero es mÃ¡s bajo, quedan " + intentos + " intentos, sigue probando"); }
+			if (intentos == 0) {
+				System.out.println("No has acertado, el nÃºmero es "+ aleatorio);
 				break; }
-		}
-		if (numero == aleatorio) {
-			System.out.println("Has acertado!");
+			numero = sc.nextInt();
 		}
 	}
-}
-		
+}		
